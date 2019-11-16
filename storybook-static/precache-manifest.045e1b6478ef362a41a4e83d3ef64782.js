a006cd3cfb85f1eafef94c21b66e912c5bef68fe
uliaka <uliana.gorin95@gmail.com>
2019-11-16 17:31:06 +0200

    first commit

---
+++ storybook-static/precache-manifest.045e1b6478ef362a41a4e83d3ef64782.js
@@ -0,0 +1,15 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "251d54a2ac5b4206f15cb8ecd87d3daa",
+    "url": "iframe.html"
+  },
+  {
+    "url": "main.fd0e3151c66867e8903a.bundle.js"
+  },
+  {
+    "url": "runtime~main.fd0e3151c66867e8903a.bundle.js"
+  },
+  {
+    "url": "vendors~main.fd0e3151c66867e8903a.bundle.js"
+  }
+]);
