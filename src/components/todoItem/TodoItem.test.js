89dfe16e24d478a995b5cf138b433decbad2296f
uliaka <uliana.gorin95@gmail.com>
2019-11-16 20:16:31 +0200

    Cereated tests

---
+++ src/components/todoItem/TodoItem.test.js
@@ -0,0 +1,28 @@
+import React from 'react'
+import { render, fireEvent } from '@testing-library/react'
+import TodoItem from './TodoItem';
+import '@testing-library/jest-dom/extend-expect'
+
+describe('TodoItem', () => {
+  const props = {
+    title: 'item one',
+    commentsCount: 56,
+    onDelete: jest.fn(),
+    onOpen: () => {
+    },
+    active: true,
+  }
+  test('should display title properly', () => {
+    const { getByText } = render(<TodoItem {...props} />)
+    expect(getByText(props.title)).toBeInTheDocument()
+  })
+  test('should display comments count properly', () => {
+    const { getByText } = render(<TodoItem {...props} />)
+    expect(getByText(`${props.commentsCount}`)).toBeInTheDocument()
+  })
+  test('should invoke onDelete callback', () => {
+    const { getByText } = render(<TodoItem {...props} />)
+    fireEvent.click(getByText('Delete'))
+    expect(props.onDelete.mock.calls.length).toBe(1)
+  })
+});
